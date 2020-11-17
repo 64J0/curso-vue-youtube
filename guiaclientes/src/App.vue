8eb0f49a75efb06359610af5c5d406b5b5b7866f
vgajo <vgajo@soujmv.com>
2020-11-17 16:47:37 -0300

    Lidando com v-for

--- guiaclientes/src/App.vue
+++ guiaclientes/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <Cliente></Cliente>
+    <div v-for="cliente in clientes" :key="cliente.id">
+      <Cliente :cliente="cliente"></Cliente>
+    </div>
   </div>
 </template>
 
@@ -9,6 +11,33 @@ import Cliente from "./components/Cliente";
 
 export default {
   name: "App",
+  data() {
+    return {
+      clientes: [
+        {
+          id: 1,
+          nome: "Vinícius Gajo",
+          descricao: "Um dois três",
+          idade: 23,
+          premium: true,
+        },
+        {
+          id: 2,
+          nome: "Vinícius Marques",
+          descricao: "Quatro cinco seis",
+          idade: 32,
+          premium: false,
+        },
+        {
+          id: 3,
+          nome: "Vinícius Oliveira",
+          descricao: "Sete oito nove",
+          idade: 2233,
+          premium: false,
+        },
+      ],
+    };
+  },
   components: {
     Cliente,
   },
--- guiaclientes/src/components/Cliente.vue
+++ guiaclientes/src/components/Cliente.vue
@@ -1,27 +1,49 @@
 <template>
-  <div>
-    <input type="text" v-model="nome" />
-    <h2>Nome do cliente: {{ nome }}</h2>
-    <h3>Idade do cliente: {{ idade }}</h3>
+  <div :class="{ cliente: true, 'cliente-premium': cliente.premium }">
+    <h4>Nome: {{ cliente.nome }}</h4>
     <hr />
+    <p>Descricao: {{ cliente.descricao }}</p>
+    <hr />
+    <p>Idade: {{ cliente.idade }}</p>
+    <hr />
+    <h4>Edição:</h4>
+    <span>Nome: </span>
+    <input type="text" v-model="cliente.nome" />
+    <span>Idade: </span>
+    <input type="text" v-model="cliente.idade" />
   </div>
 </template>
 
 <script>
 export default {
+  props: {
+    // descricao: String,
+    // idade: Number,
+    // premium: Boolean,
+    cliente: Object,
+  },
   data() {
     return {
       nome: "Vinícius Gajo",
       numero: "123",
-      email: "vinicius@gajo.com.br",
-      idade: 23,
     };
   },
 };
 </script>
 
 <style scoped>
-div {
+.cliente {
+  margin: 20px auto;
+  padding: 15px;
+  border-radius: 8px;
   text-align: center;
+  max-width: 500px;
+  max-height: 500px;
+  background-color: #1f4;
+}
+
+.cliente-premium {
+  background-color: black;
+  color: gold;
 }
 </style>
