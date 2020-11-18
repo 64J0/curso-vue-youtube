dc3855481e07bb70527618235542fe345eb274c6
vgajo <vgajo@soujmv.com>
2020-11-18 17:14:35 -0300

    Iniciando o clone da landing page do vuejs com vuejs

---
+++ vuepageclone/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <Header />
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+  },
+};
+</script>
+
+<style>
+*,
+input {
+  box-sizing: border-box;
+}
+
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+input {
+  border: none;
+}
+
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
